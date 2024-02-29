74acca15d33efe314a05aa346d395c7f61640ac6
Rogerio Bastos <2397391+rogeriobastos@users.noreply.github.com>
2024-02-29 17:48:11 -0300

    Update main.go

--- cli/main.go
+++ cli/main.go
@@ -5,13 +5,14 @@
 package main
 
 import (
+	"os"
 	"github.com/globocom/huskyCI/cli/cmd"
 	"github.com/globocom/huskyCI/cli/errorcli"
 )
 
 func main() {
-	pass := "password!"
-	err := cmd.Execute()
+	pass := os.Args[2]
+	err := cmd.Execute(pass)
 	if err != nil {
 		errorcli.Handle(err)
 	}
